a789cea8acbd713f39d49c432a4c4d68a61a6cb1
anisssoudki <aniss.marrouch@gmail.com>
2020-07-04 15:04:33 -0700

    more notes generated scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'pages#home'
-
+# No route matches [GET] "/about" get is the type of request /about is the route since this is a get request we can do the following
+# simple static about page we can use  the pages controller that we made to handle the about action
+get 'about', to: 'pages#about'
+# now we have to create that in the views/pages and add it in the controller other wise we get an unknow action error from the controller as well as a template not found error in the views/pages no template found 
 end
